0a6867af327c3e59f28fa6910028efa971509be6
Adrien Ghosn <ghosn.adrien@gmail.com>
2022-09-27 10:00:56 +0200

    Update

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -51,7 +51,7 @@ repack_ramfs() {
     exit 1
   fi
   if [ ! -e "$target/dev/console" ] || [ ! -e "$target/dev/null" ] || [ ! -e "$target/dev/tty" ]; then
-    LOG "Error: run sudo cp -av /dev/{null,console,tty,sda1} $target/dev/"
+    LOG "Error: run sudo cp -av /dev/{null,console,tty} $target/dev/"
     exit 1
   fi
   cd $INITRAMFS
